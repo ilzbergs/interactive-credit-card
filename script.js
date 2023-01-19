eb5886da1aa4de652e716d556c1d69e231353c59
Toms Ilzbergs <ilzbergs@inbox.lv>
2023-01-19 17:16:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const inputName = document.getElementById('name')
+const inputNumber = document.getElementById('number');
+const inputMonth = document.getElementById('month');
+const inputYear = document.getElementById('year');
+const inputCvc = document.getElementById('cvc');
+const firstStage = document.getElementById("first-stage");
+const secondStage=document.getElementById('second-stage')
+
+
+
+inputName.onkeyup = function () {
+    document.getElementById('valueName').innerHTML = inputName.value;
+};
+
+inputNumber.onkeyup = function () {
+    document.getElementById('valueNumber').innerHTML = inputNumber.value.match(/.{1,4}/g).join(' ');
+
+};
+
+inputMonth.onkeyup = function () {
+    document.getElementById('valueMonth').innerHTML = inputMonth.value;
+};
+
+inputYear.onkeyup = function () {
+    document.getElementById('valueYear').innerHTML = inputYear.value;
+};
+
+inputCvc.onkeyup = function () {
+    document.getElementById('valueCvc').innerHTML = inputCvc.value;
+};
+
+
+document.getElementsByClassName("input-cvc")[0].addEventListener("input", cvc);
+function cvc() {
+    this.value = this.value.replace(/[^\d]/, '');
+
+}
+
+document.getElementsByClassName("input-year")[0].addEventListener("input", year);
+function year() {
+    this.value = this.value.replace(/[^\d]/, '');
+}
+
+document.getElementsByClassName("input-month")[0].addEventListener("input", month);
+function month() {
+    this.value = this.value.replace(/[^\d]/, '');
+
+}
+
+function myFunction() {
+     firstStage.style.display="none"
+   secondStage.style.display="flex"
+}
